f89a6482f2fff20aeda289a44a510330d5d5cd4d
krish-3011 <krishpatel.co22d2@scet.ac.in>
2024-07-28 14:04:25 +0530

    commit

--- routes/staticRoute.js
+++ routes/staticRoute.js
@@ -22,7 +22,7 @@ router.get("/home",async (req,res,next) => {
     const enrollment_no = `ET${year}BT${dept}.{3}`;
 
     let statObj = { total : 0, intrested : 0, placed : 0 };
-    let students = await Student.find({Enrollment_no :{$regex : enrollment_no}});
+    let students = await Student.find({enrollment_no :{$regex : enrollment_no}});
     console.log(students)
     if(students.length > 0){
         for(let student of students){
