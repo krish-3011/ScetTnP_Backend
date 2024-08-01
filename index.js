23c15505abffaace73588d080897a2034d1481a5
krish-3011 <krishpatel.co22d2@scet.ac.in>
2024-08-01 18:12:19 +0530

    multer implementation

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.use("/company", company);
 
 // Error handling
 app.use((err, req, res, next) => {
-    res.status(err.status).json({ message: err.message });
+    res.status(err.status || 500 ).json({ message: err.message || "server error"});
 });
 
 // Start server
