69816a12f2b5c8eb184a64b4462ecabd371a3974
krish-3011 <krishpatel.co22d2@scet.ac.in>
2024-08-02 15:19:19 +0530

    popluate

--- routes/auth.js
+++ routes/auth.js
@@ -12,11 +12,17 @@ router.use(cors({
     credentials: true // Allow cookies to be sent with requests
 }));
 
+router.use(session({
+    secret: 'scett', // Replace with your secret key
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // Set to true if using HTTPS
+}));
 // Authenticate user
 router.post("/", wrapAsync(async (req, res) => {
     let { enrollmentNo, birthDate } = req.body;
 
-    let profile = await Student.findOne({ enrollment_no: enrollmentNo }).populate('applied');
+    let profile = await Student.findOne({ enrollment_no: enrollmentNo });
     if (!profile) {
         return res.status(404).json({ message: "Student not found" });
     }
@@ -26,6 +32,7 @@ router.post("/", wrapAsync(async (req, res) => {
     birthDate = new Date(birthDate);
 
     if (profile.birth_date.toISOString().slice(0, 10) === birthDate.toISOString().slice(0, 10)) {
+        req.session.user = profile; // Store user in session
         res.status(200).json(profile);
     } else {
         res.status(401).json({ message: "Wrong credentials" });
