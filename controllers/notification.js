48706c14c700fe345e080ddc95063b356b5995dc
krish-3011 <krishpatel.co22d2@scet.ac.in>
2024-08-05 17:57:26 +0530

    /notification route

--- controllers/notification.js
+++ controllers/notification.js
@@ -1,51 +1,80 @@
+const express = require("express");
+const router = express.Router();
+const mongoose = require("mongoose");
 const Notificaton = require("../schema/model/notificationSchema.js");
+const wrapAsync = require("../utils/wrapAsync.js");
 
+// Index Route
 const indexRoute = async (req, res) => {
     let notification = await Notificaton.find({});
     res.status(200).json({ notification });
 };
 
+// New Route
 const newRoute = async (req, res) => {
+    try {
+        let notificationData = req.body;
 
-    let notificationData = req.body;
-    
-    const notification = new Notificaton({
-        // sender : notificationData.sender,
-        message : notificationData.message
-    });
+        // Log the incoming data for debugging
+        console.log("Received data:", notificationData);
 
-    await notification.save();
-    res.status(201).json({ message: "New company saved" });
+        if (!notificationData.message) {
+            return res.status(400).json({ error: "Message is required" });
+        }
+
+        const notification = new Notificaton({
+            message: notificationData.message
+        });
+
+        await notification.save();
+        res.status(201).json({ message: "New notification saved" });
+    } catch (error) {
+        console.error("Error saving notification:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
 };
 
+// Show Route
 const showRoute = async (req, res, next) => {
     let { id } = req.params;
-    let notification = await Notificaton.findOne({ _id : id});
+    let notification = await Notificaton.findOne({ _id: id });
     if (!notification) {
-        let err = new Error("Notification not found. ");
+        let err = new Error("Notification not found.");
         err.status(400);
         throw err;
     }
     res.status(200).json(notification);
 };
 
+// Update Route
 const updateRoute = async (req, res) => {
     let { id } = req.params;
     let updateData = req.body;
 
-    let notification = await Notificaton.findOne({ _id : id});
+    let notification = await Notificaton.findOne({ _id: id });
     if (!notification) {
-        let err = new Error("Notification not found. ");
+        let err = new Error("Notification not found.");
         err.status(400);
         throw err;
     }
 
     // Make sure to only update fields provided
-    let updatedCompany = await Company.findByIdAndUpdate(id, { $set: updateData }, { new: true });
-    res.status(200).json({ message: "Company updated successfully", updatedCompany });
+    let updatedNotification = await Notificaton.findByIdAndUpdate(id, { $set: updateData }, { new: true });
+    res.status(200).json({ message: "Notification updated successfully", updatedNotification });
 };
 
+// Delete Route
 const deleteRoute = async (req, res) => {
+    let { id } = req.params;
+    await Notificaton.findByIdAndDelete(id);
+    res.status(200).json({ message: "Notification deleted successfully" });
 };
 
-module.exports = {indexRoute , newRoute, showRoute ,updateRoute , deleteRoute}
+// Router setup
+router.get("/", wrapAsync(indexRoute));
+router.post("/", wrapAsync(newRoute));
+router.get("/:id", wrapAsync(showRoute));
+router.patch("/:id", wrapAsync(updateRoute));
+router.delete("/:id", wrapAsync(deleteRoute));
+
+module.exports = router;
