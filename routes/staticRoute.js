a44eb77e3175e31b30cf402ca65906bf8f0dff2f
Mann Patel <mannpatel.co22d2@scet.ac.in>
2024-12-26 15:33:42 +0530

    statRoute.js -> console.log removed

--- routes/staticRoute.js
+++ routes/staticRoute.js
@@ -59,7 +59,7 @@ router.get("/home",async (req,res,next) => {
         
         currentYear -= 1;
     }
-    console.log(statObj);
+    S
 
     // students = await Su_student.find({enrollment_no :{$regex : SU_enrollment_no}}).populate('selected.offer');
 
