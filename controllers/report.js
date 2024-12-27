ecb076b67f6e84bbb41c177595d2d34ade3fc0aa
Mann Patel <mannpatel.co22d2@scet.ac.in>
2024-12-27 14:45:28 +0530

    Final Report-> removed print

--- controllers/report.js
+++ controllers/report.js
@@ -128,7 +128,6 @@ const groupByCompany = (data) => {
     return data.reduce((result, currentValue) => {
         // Check if the current value has the specified key
         if (currentValue[key]) {
-            console.log(currentValue[key]);
             currentValue[key].forEach(groupKey => {
                 // Initialize the group if it doesn't exist
                 if (!result[groupKey.company]) {
