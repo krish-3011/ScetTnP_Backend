deaf2535393a4f9f7374ae6f9ba4fc254f23e820
Mann Patel <mannpatel.co22d2@scet.ac.in>
2025-01-10 13:14:31 +0530

    cors

--- index.js
+++ index.js
@@ -22,11 +22,11 @@ dotEnv.config();
 const DB_URL = process.env.DB_URL;
 const PORT = process.env.PORT;
 
-// Middleware
-// app.use(cors({
-//     origin: ['https://scettnp-frontend.onrender.com','http://localhost:3000','http://localhost:8080'], // Specify your frontend origin
-//     credentials: true // Allow cookies to be sent with requests
-// }));
+Middleware
+app.use(cors({
+    origin: ['https://scettnp-frontend.onrender.com','http://localhost:3000','http://localhost:8080'], // Specify your frontend origin
+    credentials: true // Allow cookies to be sent with requests
+}));
 
 
 // Connect to the database
